ac920c59317239c23dce7c82d01960caa5e2b534
voidrlm <varjun609@gmail.com>
2023-09-23 18:11:14 +0530

    Updated music data static file

--- src/resources/musicData.js
+++ src/resources/musicData.js
@@ -4,8 +4,282 @@ export const musicDB = [
     title: "Starboy",
     artist: "The Weeknd",
     album: "Starboy",
-    src: "https://pagalworld.social/files/download/id/6334",
+    src: "https://www.nobadsong.com/wp-content/uploads/2023/08/The-Weeknd-Starboy.mp3",
     art: "https://upload.wikimedia.org/wikipedia/en/3/39/The_Weeknd_-_Starboy.png",
+    lyrics: [
+      {
+        time: 15.3,
+        text: "I'm tryna put you in the worst mood, ah",
+      },
+      {
+        time: 17.7,
+        text: "P1 cleaner than your church shoes, ah",
+      },
+      {
+        time: 20.3,
+        text: "Milli point two just to hurt you, ah",
+      },
+      {
+        time: 22.8,
+        text: "All red Lamb' just to ***** you, ah",
+      },
+      {
+        time: 25.4,
+        text: "None of these toys on lease too, ah",
+      },
+      {
+        time: 27.8,
+        text: "Made your whole year in a week too, yah",
+      },
+      {
+        time: 30.4,
+        text: "Main ***** outta your league too, ah",
+      },
+      {
+        time: 33.2,
+        text: "Side ***** outta your league too, ah",
+      },
+      {
+        time: 35.7,
+        text: "House so empty, need a centerpiece",
+      },
+      {
+        time: 38.5,
+        text: "Twenty racks a table, cut from ebony",
+      },
+      {
+        time: 41.2,
+        text: "Cut that ivory into skinny pieces",
+      },
+      {
+        time: 43.4,
+        text: "Then she clean it with her face, man, I love my baby, ah",
+      },
+      {
+        time: 46.4,
+        text: "You talkin' money, need a hearin' aid",
+      },
+      {
+        time: 48.6,
+        text: "You talkin' 'bout me, I don't see the shade",
+      },
+      {
+        time: 51.2,
+        text: "Switch up my style, I take any lane",
+      },
+      {
+        time: 53.7,
+        text: "I switch up my cup, I kill any pain",
+      },
+      {
+        time: 56.2,
+        text: "(Ha-ha-ha-ha-ha, ha-ha-ha-ha-ha)",
+      },
+      {
+        time: 60.4,
+        text: "Look what you've done",
+      },
+      {
+        time: 61.6,
+        text: "(Ha-ha-ha-ha-ha, ha-ha-ha-ha-ha)",
+      },
+      {
+        time: 65.2,
+        text: "I'm a ************* starboy",
+      },
+      {
+        time: 67.2,
+        text: "(Ha-ha-ha-ha-ha, ha-ha-ha-ha-ha)",
+      },
+      {
+        time: 70.8,
+        text: "Look what you've done",
+      },
+      {
+        time: 72.0,
+        text: "(Ha-ha-ha-ha-ha, ha-ha-ha-ha-ha)",
+      },
+      {
+        time: 74.6,
+        text: "I'm a ************* starboy",
+      },
+      {
+        time: 77.6,
+        text: "Every day, a ***** try to test me, ah",
+      },
+      {
+        time: 79.8,
+        text: "Every day, a ***** try to end me, ah",
+      },
+      {
+        time: 82.4,
+        text: "Pull off in that Roadster SV, ah",
+      },
+      {
+        time: 84.8,
+        text: "Pockets overweight, gettin' hefty, ah",
+      },
+      {
+        time: 87.4,
+        text: "Comin' for the king, that's a far cry, I",
+      },
+      {
+        time: 90.0,
+        text: "I come alive in the fall time, I",
+      },
+      {
+        time: 92.6,
+        text: "The competition, I don't really listen",
+      },
+      {
+        time: 95.1,
+        text: "I'm in the blue Mulsanne, bumpin' New Edition",
+      },
+      {
+        time: 98.2,
+        text: "House so empty, need a centerpiece",
+      },
+      {
+        time: 100.6,
+        text: "Twenty racks a table, cut from ebony",
+      },
+      {
+        time: 103.0,
+        text: "Cut that ivory into skinny pieces",
+      },
+      {
+        time: 105.1,
+        text: "Then she clean it with her face, man, I love my baby, ah",
+      },
+      {
+        time: 108.4,
+        text: "You talkin' money, need a hearin' aid",
+      },
+      {
+        time: 110.6,
+        text: "You talkin' 'bout me, I don't see the shade",
+      },
+      {
+        time: 113.1,
+        text: "Switch up my style, I take any lane",
+      },
+      {
+        time: 115.6,
+        text: "I switch up my cup, I kill any pain",
+      },
+      {
+        time: 118.1,
+        text: "(Ha-ha-ha-ha-ha, ha-ha-ha-ha-ha)",
+      },
+      {
+        time: 122.3,
+        text: "Look what you've done",
+      },
+      {
+        time: 123.5,
+        text: "(Ha-ha-ha-ha-ha, ha-ha-ha-ha-ha)",
+      },
+      {
+        time: 127.1,
+        text: "I'm a ************* starboy",
+      },
+      {
+        time: 129.1,
+        text: "(Ha-ha-ha-ha-ha, ha-ha-ha-ha-ha)",
+      },
+      {
+        time: 132.7,
+        text: "Look what you've done",
+      },
+      {
+        time: 133.9,
+        text: "(Ha-ha-ha-ha-ha, ha-ha-ha-ha-ha)",
+      },
+      {
+        time: 137.7,
+        text: "I'm a ************* starboy",
+      },
+      {
+        time: 140.7,
+        text: "Let a ***** brag Pitt",
+      },
+      {
+        time: 142.0,
+        text: "Legend of the fall, took the year like a bandit",
+      },
+      {
+        time: 144.6,
+        text: "Bought Mama a crib and a brand new wagon",
+      },
+      {
+        time: 147.1,
+        text: "Now she hit the g****ry shop lookin' lavish",
+      },
+      {
+        time: 149.6,
+        text: "Star Trek roof in that Wraith of Khan",
+      },
+      {
+        time: 152.1,
+        text: "Girls get loose when they hear this song",
+      },
+      {
+        time: 154.6,
+        text: "A hundred on the dash get me close to God",
+      },
+      {
+        time: 157.1,
+        text: "We don't pray for love, we just pray for cars",
+      },
+      {
+        time: 159.7,
+        text: "House so empty, need a centerpiece",
+      },
+      {
+        time: 162.1,
+        text: "Twenty racks a table, cut from ebony",
+      },
+      {
+        time: 164.5,
+        text: "Cut that ivory into skinny pieces",
+      },
+      {
+        time: 166.7,
+        text: "Then she clean it with her face, man, I love my baby, ah",
+      },
+      {
+        time: 170.0,
+        text: "You talkin' money, need a hearin' aid",
+      },
+      {
+        time: 172.2,
+        text: "You talkin' 'bout me, I don't see the shade",
+      },
+      {
+        time: 174.7,
+        text: "Switch up my style, I take any lane",
+      },
+      {
+        time: 177.2,
+        text: "I switch up my cup, I kill any pain",
+      },
+      {
+        time: 179.7,
+        text: "(Ha-ha-ha-ha-ha, ha-ha-ha-ha-ha)",
+      },
+      {
+        time: 183.9,
+        text: "Look what you've done",
+      },
+      {
+        time: 185.1,
+        text: "(Ha-ha-ha-ha-ha, ha-ha-ha-ha-ha)",
+      },
+      {
+        time: 188.7,
+        text: "I'm a ************* starboy",
+      },
+    ],
   },
   {
     id: 5312,
@@ -14,6 +288,196 @@ export const musicDB = [
     album: "Astroworld",
     src: "https://pagalworld.social/files/download/id/5312",
     art: "https://upload.wikimedia.org/wikipedia/en/3/33/Travis_Scott_Butterfly_Effect.jpg",
+    lyrics: [
+      {
+        time: 10.0,
+        text: "Murda on the beat so it's not nice",
+      },
+      {
+        time: 14.6,
+        text: "For this life I cannot change",
+      },
+      {
+        time: 17.3,
+        text: "Hidden Hills, deep off in the main",
+      },
+      {
+        time: 20.6,
+        text: "M&M's, sweet like candy cane",
+      },
+      {
+        time: 23.9,
+        text: "Drop the top, pop it let it bang (Pop it, pop it)",
+      },
+      {
+        time: 28.3,
+        text: "For this life I cannot change",
+      },
+      {
+        time: 30.9,
+        text: "Hidden Hills, deep off in the main",
+      },
+      {
+        time: 34.2,
+        text: "M&M's sweet like candy cane",
+      },
+      {
+        time: 37.6,
+        text: "Drop the top, pop it let it bang (Pop it, pop it)",
+      },
+      {
+        time: 41.9,
+        text: "Drop the top play hide and seek",
+      },
+      {
+        time: 44.3,
+        text: "Jump inside, jump straight to the leak",
+      },
+      {
+        time: 47.8,
+        text: "Take a sip, feel just how I breathe",
+      },
+      {
+        time: 51.2,
+        text: "On freeway, but no ain't nothin' free (straight up)",
+      },
+      {
+        time: 55.6,
+        text: "Bend laws, bend lanes",
+      },
+      {
+        time: 57.8,
+        text: "Been bustin' bills, but still ain't nothin' change",
+      },
+      {
+        time: 61.3,
+        text: "You in the mob soon as you rock the chain",
+      },
+      {
+        time: 64.5,
+        text: "She caught the waves just thumbin' through my braids (alright)",
+      },
+      {
+        time: 68.2,
+        text: "Heatin' up, baby, I'm just heatin' up (it's lit)",
+      },
+      {
+        time: 71.6,
+        text: "Fiji lover, now they indeed that is impressive",
+      },
+      {
+        time: 75.0,
+        text: "Fill his cup now you know how they keep me up",
+      },
+      {
+        time: 78.4,
+        text: "Icy love, icy like a hockey puck",
+      },
+      {
+        time: 82.7,
+        text: "For this life I cannot change",
+      },
+      {
+        time: 85.3,
+        text: "Hidden Hills, deep off in the main",
+      },
+      {
+        time: 88.7,
+        text: "M&M's sweet like candy cane",
+      },
+      {
+        time: 92.0,
+        text: "Drop the top, pop it let it bang (Pop it, pop it)",
+      },
+      {
+        time: 96.4,
+        text: "For this life I cannot change",
+      },
+      {
+        time: 99.1,
+        text: "Hidden Hills, deep off in the main",
+      },
+      {
+        time: 102.4,
+        text: "M&M's sweet like candy cane",
+      },
+      {
+        time: 105.6,
+        text: "Drop the top, pop it let it bang (Pop it, pop it)",
+      },
+      {
+        time: 109.0,
+        text: "All the ones, all the chains piled on the mantle",
+      },
+      {
+        time: 111.3,
+        text: "All the dogs, all the dogs low creep right ****** me in the Phantom",
+      },
+      {
+        time: 114.7,
+        text: "Never go, never go dip on this estates santana",
+      },
+      {
+        time: 117.7,
+        text: "Yeah, run it back turn the lights on when I hit up Green Lantern",
+      },
+      {
+        time: 122.1,
+        text: "Yeah, fly the broads, fly the dogs down to Atlanta",
+      },
+      {
+        time: 124.6,
+        text: "Yeah, in the cut in Medusa lay low I might be",
+      },
+      {
+        time: 127.9,
+        text: "Yeah, roll up help me calm down when I move in high s***d",
+      },
+      {
+        time: 131.3,
+        text: "Yeah, If I send one need the text back 'cause you know what I need",
+      },
+      {
+        time: 135.8,
+        text: "Oh please (oh please)",
+      },
+      {
+        time: 137.6,
+        text: "Oh me (oh me)",
+      },
+      {
+        time: 139.3,
+        text: "Oh my (oh my)",
+      },
+      {
+        time: 140.8,
+        text: "We been movin', we been movin' for some time (Alright!)",
+      },
+      {
+        time: 144.5,
+        text: "Flexin', flexin' try to exercise",
+      },
+      {
+        time: 146.6,
+        text: "Exercise, exercise, exercise, exercise",
+      },
+      {
+        time: 151.6,
+        text: "For this life I cannot change",
+      },
+      {
+        time: 154.5,
+        text: "Hidden Hills, deep off in the main",
+      },
+      {
+        time: 157.7,
+        text: "M&M's sweet like candy cane",
+      },
+      {
+        time: 161.0,
+        text: "Drop the top, pop it let it bang (Pop it, pop it)",
+      },
+    ],
   },
   {
     id: 5355,
@@ -22,5 +486,259 @@ export const musicDB = [
     album: "Trilogy",
     src: "https://24naijamusicfiles.com/wp-content/uploads/2021/03/The_Weeknd_-_The_Morning_Original_.mp3",
     art: "https://cdns-images.dzcdn.net/images/cover/0f834188957ef50cb73c9db3115a7827/350x350.jpg",
+    lyrics: [
+      {
+        time: 26.1,
+        text: "I'm ******* gone right now",
+      },
+      {
+        time: 31.4,
+        text: '"Listen, listen"',
+      },
+      {
+        time: 35.8,
+        text: "From the morning to the evening",
+      },
+      {
+        time: 38.1,
+        text: "Complaints from the tenants",
+      },
+      {
+        time: 40.2,
+        text: "Got the walls kicking like they six months pregnant",
+      },
+      {
+        time: 43.9,
+        text: "Drinking Alizé with our cereal for breakfast",
+      },
+      {
+        time: 47.6,
+        text: "Girls calling cabs at dawn quarter to seven",
+      },
+      {
+        time: 51.6,
+        text: "Sky's getting cold, we're flying from the north",
+      },
+      {
+        time: 55.7,
+        text: "Rockin' with our city like a sold out show",
+      },
+      {
+        time: 59.7,
+        text: "House full of pros that specialize in the ho'in'",
+      },
+      {
+        time: 63.8,
+        text: "Make that money rain as they taking off they clothes",
+      },
+      {
+        time: 68.0,
+        text: "Order plane tickets",
+      },
+      {
+        time: 69.9,
+        text: "Cali is the mission",
+      },
+      {
+        time: 71.8,
+        text: "Visit every month like I'm split life living",
+      },
+      {
+        time: 75.9,
+        text: "Let the world listen",
+      },
+      {
+        time: 77.7,
+        text: "If a hater's caught slipping",
+      },
+      {
+        time: 79.6,
+        text: "Then my ****** stay tight",
+      },
+      {
+        time: 81.8,
+        text: "Got my back like Pippen",
+      },
+      {
+        time: 83.8,
+        text: "Fast life gripping",
+      },
+      {
+        time: 85.8,
+        text: "Yeah, we still tippin'",
+      },
+      {
+        time: 87.8,
+        text: "Codeine cups paint a picture so *****",
+      },
+      {
+        time: 91.8,
+        text: "Fakes try to mimic",
+      },
+      {
+        time: 94.0,
+        text: "Get girls timid",
+      },
+      {
+        time: 95.7,
+        text: "But ****** closed doors they get poles so rigid",
+      },
+      {
+        time: 100.2,
+        text: "All that money, the money is the motive",
+      },
+      {
+        time: 103.7,
+        text: "All that money, the money is the motive",
+      },
+      {
+        time: 107.5,
+        text: "All that money, the money she be folding",
+      },
+      {
+        time: 111.5,
+        text: "Girl, put in work, girl, girl, put in work",
+      },
+      {
+        time: 115.5,
+        text: "Girl, put in work, girl, girl, put in work",
+      },
+      {
+        time: 119.9,
+        text: "Girl, put in work",
+      },
+      {
+        time: 124.0,
+        text: "Push it to the limit",
+      },
+      {
+        time: 125.7,
+        text: "Push it through the pain",
+      },
+      {
+        time: 127.7,
+        text: "I push it for the ******** like a ****** to the game",
+      },
+      {
+        time: 131.6,
+        text: "A ****** to that money",
+      },
+      {
+        time: 133.8,
+        text: "A ****** to the fame",
+      },
+      {
+        time: 135.8,
+        text: "So this my only chance",
+      },
+      {
+        time: 137.3,
+        text: "And when I'm over only pray",
+      },
+      {
+        time: 139.6,
+        text: "That I flow from the bottom",
+      },
+      {
+        time: 141.7,
+        text: "Closer to the top",
+      },
+      {
+        time: 143.8,
+        text: "The higher that I climb",
+      },
+      {
+        time: 145.4,
+        text: "The harder I'ma drop",
+      },
+      {
+        time: 147.6,
+        text: "These **************** tryna hold on to their credit",
+      },
+      {
+        time: 151.4,
+        text: "So I tell them use a debit",
+      },
+      {
+        time: 153.3,
+        text: "Watch they image start to lessen",
+      },
+      {
+        time: 155.4,
+        text: "I warn them like discretion",
+      },
+      {
+        time: 157.6,
+        text: "Why these ****** testing?",
+      },
+      {
+        time: 159.7,
+        text: "Always ******* testing",
+      },
+      {
+        time: 161.6,
+        text: "Why these ****** testing?",
+      },
+      {
+        time: 163.7,
+        text: "**** that I got them on straight bar hopping",
+      },
+      {
+        time: 167.6,
+        text: "To the music of the ambiance",
+      },
+      {
+        time: 169.7,
+        text: "Get **** popping",
+      },
+      {
+        time: 171.9,
+        text: "Zombies of the night (Zombies of the night)",
+      },
+      {
+        time: 174.4,
+        text: "****** ain't talking if they hyping to the crew",
+      },
+      {
+        time: 177.5,
+        text: "Get it in like pockets",
+      },
+      {
+        time: 179.5,
+        text: "Downtown loving",
+      },
+      {
+        time: 181.7,
+        text: "When the moon coming",
+      },
+      {
+        time: 183.7,
+        text: "Only place to find b*******s and**********",
+      },
+      {
+        time: 188.0,
+        text: "All that money, the money is the motive",
+      },
+      {
+        time: 191.7,
+        text: "All that money, the money is the motive",
+      },
+      {
+        time: 195.7,
+        text: "All that money, the money she be folding",
+      },
+      {
+        time: 199.9,
+        text: "Girl, put in work, girl, girl, put in work",
+      },
+      {
+        time: 203.7,
+        text: "Girl, put in work, girl, girl, put in work",
+      },
+      {
+        time: 207.6,
+        text: "Girl, put in work",
+      },
+    ],
   },
 ];
