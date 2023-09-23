30c30814db5ae4a4ee5d2aa9d9cdcb1930855b20
voidrlm <varjun609@gmail.com>
2023-09-23 15:22:11 +0530

    Implementation to play and pause audio file

--- src/components/musicPlayer/musicPlayer.js
+++ src/components/musicPlayer/musicPlayer.js
@@ -1,27 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./musicPlayer.css";
 import { IconContext } from "react-icons";
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+import { musicDB } from "../../resources/musicData";
 const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
   const togglePlay = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
     setIsPlaying(!isPlaying);
   };
 
   return (
     <div className="music-player">
+      <audio ref={audioRef} src={musicDB[0].url}></audio>
       <div className="component">
-        <h2 className="playerTitle">Playing Now</h2>
+        <h2 className="playerTitle">{musicDB[0].album}</h2>
         <div className="musicCover">
-          <img
-            className="albumArtImage"
-            src="https://upload.wikimedia.org/wikipedia/en/3/39/The_Weeknd_-_Starboy.png"
-          />
+          <img className="albumArtImage" src={musicDB[0].art} />
         </div>
         <div className="musicDetails">
-          <h3 className="title">Starboy</h3>
-          <p className="subTitle">The Weeknd</p>
+          <h3 className="title">{musicDB[0].title}</h3>
+          <p className="subTitle">{musicDB[0].artist}</p>
         </div>
         <div className="musicControls">
           <button className="playButton">
