69dfa75c556062d7e73d18029269a456a9c9f83a
voidrlm <varjun609@gmail.com>
2023-09-23 15:39:46 +0530

    Implementation to play next and previous songs

--- src/components/musicPlayer/musicPlayer.js
+++ src/components/musicPlayer/musicPlayer.js
@@ -7,6 +7,8 @@ import { musicDB } from "../../resources/musicData";
 const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(null);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+
   const togglePlay = () => {
     if (isPlaying) {
       audioRef.current.pause();
@@ -15,39 +17,67 @@ const MusicPlayer = () => {
     }
     setIsPlaying(!isPlaying);
   };
+  const playNext = () => {
+    const nextIndex = (currentSongIndex + 1) % musicDB.length;
+    setCurrentSongIndex(nextIndex);
+    audioRef.current.pause();
+
+    audioRef.current.src = musicDB[nextIndex].src;
+    audioRef.current.load();
+
+    // Use the canplaythrough event to play after the new audio is ready
+    audioRef.current.oncanplaythrough = () => {
+      audioRef.current.play();
+      audioRef.current.oncanplaythrough = null; // Remove the event listener to prevent multiple plays
+    };
+  };
+
+  const playPrev = () => {
+    const prevIndex = (currentSongIndex - 1 + musicDB.length) % musicDB.length;
+    setCurrentSongIndex(prevIndex);
+    audioRef.current.pause();
+
+    audioRef.current.src = musicDB[prevIndex].src;
+    audioRef.current.load();
+
+    audioRef.current.oncanplaythrough = () => {
+      audioRef.current.play();
+      audioRef.current.oncanplaythrough = null;
+    };
+  };
 
   return (
     <div className="music-player">
-      <audio ref={audioRef} src={musicDB[0].url}></audio>
+      <audio ref={audioRef} src={musicDB[currentSongIndex].src}></audio>
       <div className="component">
-        <h2 className="playerTitle">{musicDB[0].album}</h2>
+        <h2 className="playerTitle">{musicDB[currentSongIndex].album}</h2>
         <div className="musicCover">
-          <img className="albumArtImage" src={musicDB[0].art} />
+          <img className="albumArtImage" src={musicDB[currentSongIndex].art} />
         </div>
         <div className="musicDetails">
-          <h3 className="title">{musicDB[0].title}</h3>
-          <p className="subTitle">{musicDB[0].artist}</p>
+          <h3 className="title">{musicDB[currentSongIndex].title}</h3>
+          <p className="subTitle">{musicDB[currentSongIndex].artist}</p>
         </div>
         <div className="musicControls">
-          <button className="playButton">
+          <button className="playButton clickable" onClick={playPrev}>
             <IconContext.Provider value={{ size: "3em", color: "#000000" }}>
               <BiSkipPrevious />
             </IconContext.Provider>
           </button>
           {!isPlaying ? (
-            <button className="playButton" onClick={togglePlay}>
+            <button className="playButton clickable" onClick={togglePlay}>
               <IconContext.Provider value={{ size: "3em", color: "#000000" }}>
                 <AiFillPlayCircle />
               </IconContext.Provider>
             </button>
           ) : (
-            <button className="playButton" onClick={togglePlay}>
+            <button className="playButton clickable" onClick={togglePlay}>
               <IconContext.Provider value={{ size: "3em", color: "#000000" }}>
                 <AiFillPauseCircle />
               </IconContext.Provider>
             </button>
           )}
-          <button className="playButton">
+          <button className="playButton clickable" onClick={playNext}>
             <IconContext.Provider value={{ size: "3em", color: "#000000" }}>
               <BiSkipNext />
             </IconContext.Provider>
--- src/resources/musicData.js
+++ src/resources/musicData.js
@@ -4,14 +4,14 @@ export const musicDB = [
     title: "Starboy",
     artist: "The Weeknd",
     album: "Starboy",
-    url: "https://pagalworld.social/files/download/id/6334",
+    src: "https://pagalworld.social/files/download/id/6334",
     art: "https://upload.wikimedia.org/wikipedia/en/3/39/The_Weeknd_-_Starboy.png",
   },
   {
     id: 5312,
     title: "Butterfly Effect",
     artist: "Travis Scott",
-    url: "https://pagalworld.social/files/download/id/5312",
+    src: "https://pagalworld.social/files/download/id/5312",
     art: "https://upload.wikimedia.org/wikipedia/en/3/33/Travis_Scott_Butterfly_Effect.jpg",
   },
   {
@@ -19,7 +19,7 @@ export const musicDB = [
     title: "The Morning",
     artist: "The Weeknd",
     album: "Trilogy",
-    url: "https://24naijamusicfiles.com/wp-content/uploads/2021/03/The_Weeknd_-_The_Morning_Original_.mp3",
+    src: "https://24naijamusicfiles.com/wp-content/uploads/2021/03/The_Weeknd_-_The_Morning_Original_.mp3",
     art: "https://cdns-images.dzcdn.net/images/cover/0f834188957ef50cb73c9db3115a7827/350x350.jpg",
   },
 ];
